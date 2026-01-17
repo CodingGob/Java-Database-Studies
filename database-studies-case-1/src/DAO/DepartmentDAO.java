80f6ac9f6f8af061ed1f372cfb584fa00aa1408d
GuiSantos <gui.ssilva1994@gmail.com>
2026-01-17 11:11:40 -0300

    adding department DAO and its features

---
+++ database-studies-case-1/src/DAO/DepartmentDAO.java
@@ -0,0 +1,69 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DepartmentDAO {
+    
+    public static int sellerCount(Connection conn, String departmentName) throws SQLException {
+        // Returns the number of sellers in a given department.
+        // Returns -1 if department doesn't exist
+
+        String sql = """
+        SELECT COUNT(seller.Id) as SellerCount
+        FROM department
+        LEFT JOIN seller ON seller.DepartmentId = department.Id
+        WHERE department.Name = ?
+        GROUP BY department.Id
+        """;
+                
+        try (PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, departmentName);
+            try (ResultSet rs = stmt.executeQuery()){
+                if (!rs.next()){
+                    return -1;
+                }
+                     
+                return rs.getInt("SellerCount");
+            } catch (SQLException e) {
+            throw new SQLException("Could not execute the query.", e);
+            }
+        } catch (SQLException e) {
+            throw new SQLException("Could not prepare the statement.", e);
+        }
+    }
+
+    public static String sellerCountAll(Connection conn) throws SQLException {
+        // Returns a string listing all departments and their respective seller counts.
+
+        StringBuilder sb = new StringBuilder();
+        boolean hasResults = false;
+
+        String sql = """
+        SELECT department.Name, COUNT(seller.Id) as SellerCount
+        FROM department
+        LEFT JOIN seller ON seller.DepartmentId = department.Id
+        GROUP BY department.Id
+        """;
+        
+        try (PreparedStatement stmt = conn.prepareStatement(sql)){
+            try (ResultSet rs = stmt.executeQuery()){
+                while (rs.next()) {
+                    hasResults = true;
+                    sb.append(rs.getString("Name"));
+                    sb.append(" - ");
+                    sb.append(rs.getString("SellerCount"));
+                    sb.append("\n");
+                }
+
+                return hasResults ? sb.toString() : "No departments found.";
+            } catch (SQLException e) {
+                throw new SQLException("Could not excecute the query.", e);
+            }
+        } catch (SQLException e) {
+            throw new SQLException("Could not prepare the statement.", e);
+        }
+    }
+}
--- database-studies-case-1/src/application/App.java
+++ database-studies-case-1/src/application/App.java
@@ -1,50 +1,41 @@
 package application;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.Scanner;
 
+import DAO.DepartmentDAO;
 import connection.MySQLConnection;
 
 public class App {
     public static void main(String[] args) throws Exception {
         try (var conn = MySQLConnection.getConnection()) {
-            int howManySellers = 0;
+            int sellerCount;
             String department;
-            String sql;
             
             try (Scanner sc = new Scanner(System.in)){
                 System.out.print("Inform the name of the depatment: ");
                 department = sc.nextLine();
             }
 
-            sql = """
-                SELECT COUNT(seller.Id)
-                FROM department
-                LEFT JOIN seller ON seller.DepartmentId = department.Id
-                WHERE department.Name = ?
-                GROUP BY department.Id
-                """;
-                
-            try (PreparedStatement stmt = conn.prepareStatement(sql)){
-                stmt.setString(1, department);
-                try (ResultSet rs = stmt.executeQuery()){
-                    if (!rs.next()){
-                        System.out.println("Error: There is no '" + department + "' department.");
-                        return;
-                    }
-                     
-                    howManySellers = rs.getInt(1);
-                }
+            sellerCount = DepartmentDAO.sellerCount(conn, department);
+            switch (sellerCount){
+                case -1:
+                    System.out.println("Error: There is no '" + department + "' department.");
+                    break;
+                case 0:
+                    System.out.println("There are no sellers in the '" + department + "' department");
+                    break;
+                case 1:
+                    System.out.println("There is " + sellerCount + " seller in the '" + department + "' department.");
+                    break;
+                default:
+                    System.out.println("There are " + sellerCount + " sellers in the '" + department + "' department.");
+                    break;
             }
-                
-            if (howManySellers == 1) {
-                System.out.println("There is " + howManySellers + " seller in the '" + department + "' department.");
-            } else if (howManySellers == 0) {
-                System.out.println("There are no sellers in the '" + department + "' department");
-            } else {
-                System.out.println("There are " + howManySellers + " sellers in the '" + department + "' department.");
-            }
+            
+            System.out.println();
+            System.out.print(DepartmentDAO.sellerCountAll(conn));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
